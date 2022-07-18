afd0e21ee8a36b8ab00c9cfc30464563f079faed
Geoff Doty <n2geoff@gmail.com>
2022-07-17 23:51:24 -0400

    test place-holder

---
+++ pages/hello-riot/hello-riot.spec.js
@@ -0,0 +1,17 @@
+import HelloRiot from './hello-riot.riot';
+import {expect} from 'chai';
+import {component} from 'riot';
+
+describe('Hello Riot Page Unit Test', () => {
+    const mount = component(HelloRiot);
+
+    it('The component properties are properly rendered', () => {
+        const div = document.createElement('div')
+
+        const component = mount(div, {
+            version: 'v6.1.2'
+        });
+
+        expect(component.$('p').innerHTML).to.be.equal('v6.1.2');
+    });
+});
