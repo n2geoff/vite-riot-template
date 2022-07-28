1e61d092377696284ad67a6fdd89d0391afea1df
Geoff Doty <n2geoff@gmail.com>
2022-07-28 18:38:49 -0400

    project structure refactor

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import riot from 'rollup-plugin-riot';
 
 export default defineConfig({
-    plugins: [{...riot(), enforce: 'pre'}]
+    plugins: [{...riot(), enforce: 'pre'}],
+    publicDir: 'src/public'
 });
