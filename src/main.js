72bb631f810f0eda570192c358110e56bf98b42d
Geoff Doty <n2geoff@gmail.com>
2024-09-01 10:44:20 -0400

    add plugin support/example

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import * as riot from "riot";
 import app from "./components/app.riot";
 
 const main = {
+    // start-up actions here
     init() {
-        // start-up actions here
+        // install plugins
+        this.plugins();
 
         // mount components
         this.mount();
+    },
+
+    // extend functionality
+    plugins() {
+        riot.install((component) => {
+            // add your own features here
+            component.version = "3.0.0";
+        });
     },
 
     mount() {
